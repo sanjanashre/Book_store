7867da8517e23a3b725ec7728efb94812a88876a
limkarhabhishek <abhisheklimkarh@steinnlabs.com>
2025-02-20 11:25:47 +0530

    chore: Added todos task

--- books_store/models.py
+++ books_store/models.py
@@ -7,7 +7,7 @@ import uuid
 
 
 class Book(Base):
-    __tablename__ = "books" 
+    __tablename__ = "books"
 
     id = Column(String(255), primary_key=True, default=uuid.uuid4)
     title = Column(String, nullable=False)
@@ -16,3 +16,23 @@ class Book(Base):
     isbn = Column(String(13), nullable=True, unique=True)
     price = Column(Integer, nullable=True)
     created_at = Column(DateTime, default=datetime.now)
+    library_id = Column(String(255), nullable=True)
+
+
+# TODO:
+# Add a new model called Library
+# Add fields such as id, name, address_line_one, address_line_two, city, state, country, zip_code
+# GET /libraries - This endpoint will give me a list of all the libraries
+# POST /libraries - This endpoint allow you to add a library
+## Schema - LibraryRequestSchema
+## name, address_line_one, address_line_two, city, state, country, zip_code
+
+# TODO:
+# Add a new model called LibraryBooks
+# Add fields like library_id, book_id
+# Add API endpoints to add books to a library
+# GET /libraries/{library_id}/books - This endpoint will give me a list of all the books in a library
+# POST /libraries/{library_id}/books - This endpoint allow you to add a book to a library
+## Schema - LibraryBookRequestSchema
+# class LibraryBookRequestSchema(BaseModel):
+#   book_ids: list[uuid]
